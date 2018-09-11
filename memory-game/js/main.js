32deb70467fc44548a66c34533ed673b10d8d397
Cynthia Nguyen <cynthia@Cynthias-MacBook-Pro.local>
2018-09-10 22:39:22 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,8 +1,22 @@
 console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var checkForMatch = function (){
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+alert ('You got a match!')
+} else {
+alert ("Sorry, try again.")
+}
+}
+var flipCard = function (cardId) {
+console.log("User flipped" + cards[cardId])
+cardsInPlay.push(cards[cardId])
+if (cardsInPlay.length === 2) {
+checkForMatch();
+}
+}
+flipCard(0)
+flipCard(2)
+
+
